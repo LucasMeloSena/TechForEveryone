809065c160180034fd9833d4ed727fef1161adca
LucasMeloSena <lucasmelosena@gmail.com>
2023-12-28 21:25:32 -0300

    add the connection with the database in the `database.js` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,7 @@
-function status(request, response) {
-  response.status(200).json({ message: "Página de status ok" })
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  response.status(200).json({ message: "Página de status ok" });
 }
 
-export default status
+export default status;
