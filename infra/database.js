02a312b0db35dbe5f3197b8704e7912909f15317
LucasMeloSena <lucasmelosena@gmail.com>
2023-12-28 21:37:15 -0300

    move `.env` file

--- infra/database.js
+++ infra/database.js
@@ -6,14 +6,14 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
 }
 
 export default {
-  query: query
-}
+  query: query,
+};
