b3c28fdac2f941ec25be6541b52205e4b488cdc5
Lucas Sena <93053816+LucasMeloSena@users.noreply.github.com>
2023-12-31 17:25:00 +0000

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   response.status(200).json({ message: "Página de status ok" });
