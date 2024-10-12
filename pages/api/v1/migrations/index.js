1f9bed008a5e9f2ef5776c9244da41038028a3d3
Lucas Sena <lucasmelosena@gmail.com>
2024-10-12 17:02:34 -0300

    fix: change join to resolve in /api/v1/migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import { resolve } from "node:path";
 import database from "infra/database";
 
 async function migrations(req, res) {
@@ -7,7 +7,7 @@ async function migrations(req, res) {
 
   const defaultMigrationsOptions = {
     dbClient: dbClient,
-    dir: join("infra", "migrations"),
+    dir: resolve("infra", "migrations"),
     dryRun: true,
     direction: "up",
     verbose: true,
